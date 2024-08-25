04a554e816a1f5dbcbfa413e7562c7f0b73b261e
EyuelGetachew_X <32816972+EyuReaper@users.noreply.github.com>
2024-08-25 23:58:29 +0300

    used jsx

--- example.js
+++ example.js
@@ -1,6 +1,19 @@
 class AppComponent extends React.Component {
   render() {
-    return React.createElement("h1", { className: "header" }, "hello world");
+    //written using jsx *only returns the shadow DOM
+    return (
+      <section classname="sitewrap">
+        <h1>header</h1>
+        <p>lorem</p>
+      </section>
+    );
+
+    return React.createElement(
+      "section",
+      { className: "site-wrap" },
+      React.createElement("h1", null, "Header"),
+      React.createElement("P", null, "lorim")
+    );
   }
 }
 ReactDOM.render(
